97c57af0e6628cb236c074a97721259d68ec0512
Marine DELAHAYE <madelaha@e2r6p11.clusters.42paris.fr>
2023-02-20 16:35:53 +0100

    clean header3

--- inc/utils.hpp
+++ inc/utils.hpp
@@ -6,7 +6,7 @@
 /*   By: madelaha <madelaha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 11:48:32 by ghanquer          #+#    #+#             */
-/*   Updated: 2023/02/20 12:34:34 by madelaha         ###   ########.fr       */
+/*   Updated: 2023/02/20 16:35:25 by madelaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,18 @@
 #include "../inc/Server.hpp"
 #include <string>
 
-std::string                                 itos(int n);
-void	                                    insert_all(std::vector<unsigned char> &my_vec, std::string to_insert);
 std::vector<std::vector<unsigned char> >	splitOnComa(std::vector<unsigned char> str);
-int		my_compare(std::vector<unsigned char> my_vec, std::string str);
-void	free_fun(Server &my_server);
-
-std::string                                 v_to_str(std::vector<unsigned char> vector);
-
 std::vector<unsigned char>                  to_vector(std::string str);
+std::vector<unsigned char>                  concat_vectors(std::vector<unsigned char> v1, std::vector<unsigned char> v2);
+std::vector<unsigned char>                  server_response(std::string client, std::string code, std::string command, std::string to_insert);
+std::string                                 itos(int n);
+std::string                                 v_to_str(std::vector<unsigned char> vector);
+void	                                    insert_all(std::vector<unsigned char> &my_vec, std::string to_insert);
+void	                                    free_fun(Server &my_server);
 void                                        add_to_vector(std::vector<unsigned char> v, char *str);
 void                                        add_to_vector(std::vector<unsigned char>* v, std::string str);
 void                                        add_to_vector(std::vector<unsigned char>*, std::vector<unsigned char>);
-std::vector<unsigned char>                  concat_vectors(std::vector<unsigned char> v1, std::vector<unsigned char> v2);
-
 void                                        print_vector(std::vector<unsigned char> v);
-
-// Responses
-std::vector<unsigned char>                  server_response(std::string client, std::string code, std::string command, std::string to_insert);
+int		                                    my_compare(std::vector<unsigned char> my_vec, std::string str);
 
 #endif
