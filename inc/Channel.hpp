e3d66668adb741a473c81f96ab10101525e63ca5
Marine DELAHAYE <madelaha@e2r6p11.clusters.42paris.fr>
2023-02-20 16:33:05 +0100

    clean header2

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: madelaha <madelaha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 17:48:20 by ghanquer          #+#    #+#             */
-/*   Updated: 2023/02/20 12:48:56 by madelaha         ###   ########.fr       */
+/*   Updated: 2023/02/20 16:32:46 by madelaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,54 +20,49 @@
 #include "Server.hpp"
 
 class User;
+
 class Server;
 
 class Channel
 {
 	public:
-	
-		Channel(void);
-		Channel(const Channel &copy);
-		Channel(std::vector<unsigned char> chanName);
-		~Channel(void);
+			Channel(void);
+			Channel(const Channel &copy);
+			Channel(std::vector<unsigned char> chanName);
+			~Channel(void);
 		
-		Channel &	operator=(const Channel & src);
-		bool		operator!=(const Channel &) const;
-		std::vector<unsigned char>	getChanName(void) const;
-		std::list<User *>::iterator	getOpListbg(void);
-		std::list<User *>::iterator	getOpListend(void);
-		std::list<User *>::iterator	getUsrListbg(void);
-		std::list<User *>::iterator	getUsrListend(void);
-		std::list<User *>			getUsrList(void);
-		void						addUser(User *, Server&);
-		void						addUser(User *, Server&, std::vector<unsigned char>);
-		bool						isOp(User *) const;
-		void						delUser(int);
+			Channel &	operator=(const Channel & src);
+			bool		operator!=(const Channel &) const;
+		
+			std::vector<unsigned char>		getChanName(void) const;
+			std::list<User *>::iterator		getOpListbg(void);
+			std::list<User *>::iterator		getOpListend(void);
+			std::list<User *>::iterator		getUsrListbg(void);
+			std::list<User *>::iterator		getUsrListend(void);
+			std::list<User *>				getUsrList(void);
+			std::list<User *>				getUsers(void) const;
+			std::vector<unsigned char>		getTopic(void) const;
+			std::map<char, bool>			getModes(void) const;
 
-		// Getters
-		std::list<User *>			getUsers(void) const;
-	
-		std::vector<unsigned char>	 getTopic(void) const;
-		std::map<char, bool>		 getModes(void) const;
-		
-		void        setTopic(std::vector<unsigned char>);
-		bool        isOp(User usr) const;
-		std::list<User *>::iterator	findUser(std::vector<unsigned char> nick);
-
-		
+			void   setTopic(std::vector<unsigned char>);
+			
+			void						addUser(User *, Server&);
+			void						addUser(User *, Server&, std::vector<unsigned char>);
+			bool						isOp(User *) const;
+			void						delUser(int);
+			bool      					isOp(User usr) const;
+			std::list<User *>::iterator	findUser(std::vector<unsigned char> nick);
 
 	private:
 	
-		std::vector<unsigned char>	_chanName;
-		std::vector<unsigned char>	_chanPassword;
-		std::vector<unsigned char>  _topic;
-		std::map<char, bool>		_modes;	//A voir pour le string, trouver une facon de normaliser nos modes (Same pour Users) maybe un tableau toujours dans le meme ordre
-		int							_userConnected;
-		std::list<User *>			_opList;
-		std::list<User *>			_userLst;
-		std::vector<User *>			_banLst;
-
-
+			std::vector<unsigned char>	_chanName;
+			std::vector<unsigned char>	_chanPassword;
+			std::vector<unsigned char>  _topic;
+			std::map<char, bool>		_modes;	//A voir pour le string, trouver une facon de normaliser nos modes (Same pour Users) maybe un tableau toujours dans le meme ordre
+			int							_userConnected;
+			std::list<User *>			_opList;
+			std::list<User *>			_userLst;
+			std::vector<User *>			_banLst;
 };
 
 #endif
