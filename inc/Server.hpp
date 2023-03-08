aff17253a7c4deb456805d1a8bdf62d5321f819a
Iacopo GUSCETTI <iacopo.guscetti@gmail.com>
2023-03-08 19:00:45 +0100

    lol

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Server.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: madelaha <madelaha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: iguscett <iguscett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 17:39:58 by ghanquer          #+#    #+#             */
-/*   Updated: 2023/03/08 10:56:27 by ghanquer         ###   ########.fr       */
+/*   Updated: 2023/03/08 18:58:57 by iguscett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,6 +79,8 @@ class Server
 		// Channels
 		bool							channelExists(std::vector<unsigned char>&);
 		void							addNewChannel(std::vector<unsigned char>);
+		void							addNewChannel(Channel& new_channel);
+
 
 		// Channel*						findChanPtr(std::vector<unsigned char>);
 		void							send_to_client(int, std::vector<unsigned char>);
